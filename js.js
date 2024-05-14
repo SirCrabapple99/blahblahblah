be1b2037076f6d52d45c5aca431db48f5571dece
SirCrabapple99 sigma <gabejc2010@gmail.com>
2024-05-14 23:18:21 +0000

    stuff

--- js.js
+++ js.js
@@ -1,6 +1,26 @@
 let clicks = 0;
+let treesLeft = 13.505;
+var setback = 0
 function click1234() {
-        clicks += 1;
-        document.getElementById("clicks").innerHTML = clicks;
+        if (setback == 1) {
+                clicks += 0.001;
+                document.getElementById("clicks").innerHTML = (Math.round(clicks * 100) / 100);
+                document.getElementById("thousands").innerHTML = ("k");
+            }
+        else {
+            if (clicks >= 999) {
+                clicks = 1
+                setback = 1
+                document.getElementById("clicks").innerHTML = clicks;
+                document.getElementById("thousands").innerHTML = "k";
+                }
+                else {
+                    clicks += 1;
+                    document.getElementById("clicks").innerHTML = clicks;
+                }
+            
+        }
         console.log("4");
+        treesLeft -= 0.001;
+        document.getElementById("forestcount").innerHTML = Math.round(treesLeft * 10) / 10;
     }
